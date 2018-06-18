ce7dcb599c47c438faf79e4b20fca4c45eef6599
aubry prieur <aubry.prieur@gmail.com>
2018-06-18 14:01:50 +0200

    config create, index and destroy alerte publication

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,11 +1,25 @@
 class PublicationsController < ApplicationController
+  before_action :set_publication, only: [:edit, :destroy]
+  before_action :favorites
+
   def index
+    @publications = Publication.all
+    @eligibles = Eligible.all
+    @categories = Category.all
   end
 
   def new
+    @publication = Publication.new
   end
 
   def create
+    @publication = Publication.new(publication_params)
+    @publication.user = current_user
+    if @publication.save
+      redirect_to publications_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +29,22 @@ class PublicationsController < ApplicationController
   end
 
   def destroy
+    @publication.destroy
+    redirect_to publications_path
   end
+
+  private
+
+  def set_publication
+    @publication = Publication.find(params[:id])
+  end
+
+  def publication_params
+    params.require(:publication).permit(:category_id, :eligible_id)
+  end
+
+  def favorites
+    @favorite_projects = current_user.favorite_projects
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  get 'publications/index'
-
-  get 'publications/new'
-
-  get 'publications/create'
-
-  get 'publications/edit'
-
-  get 'publications/update'
-
-  get 'publications/destroy'
 
   ActiveAdmin.routes(self)
   get '/favorites', to: 'favorite_projects#index', as: 'favorites'
   resources :favorite_projects, only: [:create, :destroy]
   resources :projects
+  resources :publications
   devise_for :users
   root to: 'pages#home'
   get 'tags/:tag', to: 'projects#index', as: "tag"
