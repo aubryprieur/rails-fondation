25b286bf452c1e00971f58e688622a19f80c056b
aubry prieur <aubry.prieur@gmail.com>
2018-10-28 14:24:19 +0100

    envoi email suite inscription alerte sur une catégorie

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -7,11 +7,9 @@ class ProjectMailer < ApplicationMailer
   #
   def newproject(project)
     @project = project
-    project_eligibles = ProjectEligible.where("project_id =?", project.id).pluck(:eligible_id)
       mail(
-        to:       User.includes(:publications).where("publications.category_id =? AND publications.eligible_id =? ", @project.category_id, project_eligibles).pluck(:email),
-        subject:  "Un nouvel appel à projet sur FondaBase !"
+      to:       User.includes(:publications).where("publications.category_id =?", @project.category_id).pluck(:email),
+      subject:  "Un nouvel appel à projet sur FondaBase !"
       )
   end
-
 end
